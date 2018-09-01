9f822622bf557d35d111280af274913360aedd39
WMJ <wmj@live.cn>
2018-09-01 09:55:05 +0800

    Version 3.7: + Suppressed Smart Bar by holding Shift key + Option to show <remarks> on Super Quick Info + Rendered <code> XML Doc + Themeable Super Quick Info - Fixed #34, fixed #29 ! Other tweaks and bug fixes

--- Codist/CodistPackage.cs
+++ Codist/CodistPackage.cs
@@ -13,7 +13,7 @@ namespace Codist
 	/// <para>The project consists of the following namespace: <see cref="SyntaxHighlight"/> backed by <see cref="Classifiers"/>, <see cref="SmartBars"/>, <see cref="QuickInfo"/>, <see cref="Margins"/>, etc.</para>
 	/// </summary>
 	[PackageRegistration(UseManagedResourcesOnly = true)]
-	[InstalledProductRegistration("#110", "#112", "3.6", IconResourceID = 400)] // Information on this package for Help/About
+	[InstalledProductRegistration("#110", "#112", "3.7", IconResourceID = 400)] // Information on this package for Help/About
 	[Guid(PackageGuidString)]
 	[ProvideOptionPage(typeof(Options.General), Constants.NameOfMe, "General", 0, 0, true)]
 	[ProvideOptionPage(typeof(Options.SuperQuickInfo), CategorySuperQuickInfo, "General", 0, 0, true, Sort = 10)]
