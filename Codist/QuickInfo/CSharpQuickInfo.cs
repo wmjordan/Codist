b51f90a6edb786fbf3ec631c3696db0b0d00bcda
WMJ <wmj@live.cn>
2019-06-03 15:22:06 +0800

    - Fixed incorrect location info of namespaces in Super Quick Info - Fixed incorrect variable capture info

--- Codist/QuickInfo/CSharpQuickInfo.cs
+++ Codist/QuickInfo/CSharpQuickInfo.cs
@@ -135,9 +135,9 @@ namespace Codist.QuickInfo
 				skipTriggerPointCheck == false && node.Span.Contains(subjectTriggerPoint.Position, true) == false) {
 				goto EXIT;
 			}
-			if (node.Parent.IsKind(SyntaxKind.QualifiedName)) {
-				node = node.Parent;
-			}
+			//if (node.Parent.IsKind(SyntaxKind.QualifiedName)) {
+			//	node = node.Parent;
+			//}
 			LocateNodeInParameterList(ref node, ref token);
 			if (Config.Instance.QuickInfoOptions.MatchFlags(QuickInfoOptions.Parameter)) {
 				ShowParameterInfo(qiContent, node);
@@ -316,7 +316,7 @@ namespace Codist.QuickInfo
 				var ss = node.AncestorsAndSelf().FirstOrDefault(i => i is StatementSyntax || i is ExpressionSyntax && i.Kind() != SyntaxKind.IdentifierName);
 				if (ss != null) {
 					var df = _SemanticModel.AnalyzeDataFlow(ss);
-					var captured = ss is StatementSyntax || ss.IsKind(SyntaxKind.InvocationExpression) || ss.IsKind(SyntaxKind.ParenthesizedLambdaExpression) || ss.IsKind(SyntaxKind.SimpleLambdaExpression) ? df.CapturedInside : df.ReadInside;
+					var captured = ss is StatementSyntax || ss.IsKind(SyntaxKind.InvocationExpression) || ss.IsKind(SyntaxKind.ParenthesizedLambdaExpression) || ss.IsKind(SyntaxKind.SimpleLambdaExpression) ? df.DataFlowsIn : df.ReadInside;
 					if (captured.Length > 0) {
 						var p = new ThemedTipParagraph(KnownImageIds.ExternalVariableValue, new ThemedTipText().Append("Captured variables", true));
 						int i = 0;
