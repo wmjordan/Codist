0802d369d9ea54f099bfbb399fe73141b96ff444
WMJ <wmj@live.cn>
2022-01-05 17:32:58 +0800

    + Added test case for C# 10 value tuple assignments

--- TestProject/CS10_0.cs
+++ TestProject/CS10_0.cs
@@ -4,8 +4,8 @@ using System.Linq.Expressions;
 namespace TestProject.CS10_0;
 record struct RecordStruct
 {
-	public Address Address { get; init; } = new();
 	public string Name { get; init; }
+	public Address Address { get; init; } = new();
 	public System.Collections.Generic.Dictionary<string, object> Values { get; init; } = new();
 
 	public RecordStruct NewName(string name)
@@ -38,7 +38,15 @@ class Fields
 {
 	public string S = new('*', 3);
 	public readonly System.Collections.Generic.Dictionary<string, object> Values = new();
+
+	void DeclareInDeconstruct() {
+		var p = (1, 2);
+		(int x, int y) = p;
+		(x, int z) = p;
+		Console.WriteLine($"x={x},y={y},z={z}");
+	}
 }
+
 class Lambdas
 {
 	[Obsolete($"Call {nameof(NaturalTypesOfMethodGroups)} instead")]
