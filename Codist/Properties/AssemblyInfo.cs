b9a508e8e317ee43d5f896a2b8c5d475bd406c28
WMJ <wmj@live.cn>
2022-10-24 11:48:29 +0800

    Version 6.6

--- Codist/Properties/AssemblyInfo.cs
+++ Codist/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark(nameof(Codist))]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion(Codist.Config.CurrentVersion + ".7500")]
+[assembly: AssemblyFileVersion(Codist.Config.CurrentVersion + ".7700")]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
