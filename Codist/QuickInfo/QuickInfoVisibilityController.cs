56d05535c507ff7893ff24bc407a69ec307d4193
WMJ <wmj@live.cn>
2023-01-03 11:45:10 +0800

    + Added Shift to suppress Quick Info, if CtrlQuickInfo option was disabled

--- Codist/QuickInfo/QuickInfoVisibilityController.cs
+++ Codist/QuickInfo/QuickInfoVisibilityController.cs
@@ -19,9 +19,11 @@ namespace Codist.QuickInfo
 
 		public async Task<QuickInfoItem> GetQuickInfoItemAsync(IAsyncQuickInfoSession session, CancellationToken cancellationToken) {
 			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-			// don't show Quick Info when CtrlQuickInfo option is on and shift is not pressed
+			// hide Quick Info when:
+			//   CtrlQuickInfo option is on and shift is not pressed,
+			//   or CtrlQuickInfo is off and shift is pressed
 			if (Config.Instance.QuickInfoOptions.MatchFlags(QuickInfoOptions.CtrlQuickInfo)
-				&& Keyboard.Modifiers.MatchFlags(ModifierKeys.Shift) == false
+				^ Keyboard.Modifiers.MatchFlags(ModifierKeys.Shift)
 				// do not show Quick Info when user is hovering on the SmartBar or the SymbolList
 				|| session.TextView.Properties.ContainsProperty(SmartBars.SmartBar.QuickInfoSuppressionId)
 				|| session.TextView.Properties.ContainsProperty(Controls.ExternalAdornment.QuickInfoSuppressionId)
