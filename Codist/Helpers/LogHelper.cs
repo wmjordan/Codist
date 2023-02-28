2ef6fb50a1da50cdfaf85244215ea336ab428e80
WMJ <wmj@live.cn>
2023-02-28 15:01:11 +0800

    ! Included DEBUG condition in LogHelper methods

--- Codist/Helpers/LogHelper.cs
+++ Codist/Helpers/LogHelper.cs
@@ -9,7 +9,7 @@ namespace Codist
 {
 	/// <summary>
 	/// <para>A simple logger which logs information to the file in <see cref="LogPath"/>. The file must pre-exist before <see cref="LogPath"/> is specified.</para>
-	/// <para>By default, all log methods are simply ignored. Specify "<c>LOG</c>" in <i>conditional compilation symbols</i> of the project.</para>
+	/// <para>By default, all log methods are simply ignored. Specify "<c>LOG</c>" or "<c>DEBUG</c>" in <i>conditional compilation symbols</i> of the project.</para>
 	/// </summary>
 	static class LogHelper
 	{
@@ -22,6 +22,7 @@ namespace Codist
 		}
 
 		[Conditional("LOG")]
+		[Conditional("DEBUG")]
 		public static void Log(this string message) {
 			if (_Path != null) {
 				WriteLog(message);
@@ -29,12 +30,14 @@ namespace Codist
 			Debug.WriteLine(message);
 		}
 
+		[Conditional("LOG")]
+		[Conditional("DEBUG")]
 		static void WriteLog(string message) {
 			while (Interlocked.CompareExchange(ref _Sync, 1, 0) != 0) {
 				SpinWait.SpinUntil(() => Volatile.Read(ref _Sync) == 0);
 			}
 			try {
-				File.AppendAllText(_Path, $"{DateTime.Now.ToLongTimeString()} {message}{Environment.NewLine}");
+				File.AppendAllText(_Path, $"{DateTime.Now.ToLongTimeString()}\t{message}{Environment.NewLine}");
 			}
 			catch (SystemException) {
 				// ignore
@@ -45,11 +48,13 @@ namespace Codist
 		}
 
 		[Conditional("LOG")]
+		[Conditional("DEBUG")]
 		public static void Log(this Exception exception) {
 			Log(exception.ToString());
 		}
 
 		[Conditional("LOG")]
+		[Conditional("DEBUG")]
 		public static void ClearLog() {
 			if (File.Exists(LogPath)) {
 				try {
