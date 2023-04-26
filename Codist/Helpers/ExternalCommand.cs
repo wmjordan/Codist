6d9e4e821ee3850dd87300791a875944c85049d5
WMJ <wmj@live.cn>
2023-04-26 17:08:57 +0800

    ! Moved class ! Use MessageWindow to display error dialog box

--- Codist/Helpers/ExternalCommand.cs
+++ Codist/Helpers/ExternalCommand.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Windows;
+using System.Diagnostics;
 using System.Windows.Input;
 using Microsoft.VisualStudio.Shell;
 using R = Codist.Properties.Resources;
 
-namespace Codist.Controls
+namespace Codist
 {
 	public static class ExternalCommand
 	{
@@ -16,19 +16,19 @@ namespace Codist.Controls
 					CodistPackage.DTE.ItemOperations.Navigate(url);
 				}
 				else if (String.IsNullOrEmpty(Config.Instance.BrowserPath) == false) {
-					System.Diagnostics.Process.Start(Config.Instance.BrowserPath, String.IsNullOrEmpty(Config.Instance.BrowserParameter) ? url : Config.Instance.BrowserParameter.Replace("%u", url));
+					Process.Start(Config.Instance.BrowserPath, String.IsNullOrEmpty(Config.Instance.BrowserParameter) ? url : Config.Instance.BrowserParameter.Replace("%u", url));
 				}
 				else {
-					System.Diagnostics.Process.Start(url);
+					Process.Start(url);
 				}
 			}
 			catch (Exception ex) {
-				MessageBox.Show(R.T_FailedToLaunchBrowser + Environment.NewLine + ex.Message, nameof(Codist), MessageBoxButton.OK, MessageBoxImage.Exclamation);
+				Controls.MessageWindow.Error(R.T_FailedToLaunchBrowser + Environment.NewLine + ex.Message, nameof(Codist));
 			}
 		}
 
 		public static void OpenTaskManager() {
-			System.Diagnostics.Process.Start(Config.Instance.TaskManagerPath ?? "TaskMgr.exe", Config.Instance.TaskManagerParameter);
+			Process.Start(Config.Instance.TaskManagerPath ?? "TaskMgr.exe", Config.Instance.TaskManagerParameter);
 		}
 	}
 }
