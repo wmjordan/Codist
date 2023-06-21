251d8a32cec24277f3a18a1b74a49c762720005f
WMJ <wmj@live.cn>
2023-06-21 11:00:35 +0800

    - Remove inappropriate method

--- Codist/Helpers/TextEditorHelper.cs
+++ Codist/Helpers/TextEditorHelper.cs
@@ -76,9 +76,6 @@ namespace Codist
 				? new SnapshotSpan(snapshot, span.Start, span.Length)
 				: default;
 		}
-		public static SnapshotSpan ToSnapshotSpan(this IMappingSpan span) {
-			return span.GetSpans(span.AnchorBuffer)[0];
-		}
 		public static Span ToSpan(this TextSpan span) {
 			return new Span(span.Start, span.Length);
 		}
