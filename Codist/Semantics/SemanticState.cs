a036884d60b0b3aac7ec3c4e326e95da402585ff
WMJ <wmj@live.cn>
2023-07-04 20:59:06 +0800

    ! Remove unused using statement

--- Codist/Semantics/SemanticState.cs
+++ Codist/Semantics/SemanticState.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Text;
 
 namespace Codist
