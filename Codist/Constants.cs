cf21ee6f1d0f3dbdff63305ce05b6a2431d51324
WMJ <wmj@live.cn>
2025-06-20 19:44:22 +0800

    ! Refactor suppression constants to individual file

--- Codist/Constants.cs
+++ Codist/Constants.cs
@@ -333,15 +333,6 @@ namespace Codist
 		public const string EmptyColor = "#00000000";
 	}
 
-	public static class Suppression
-	{
-		public const string VSTHRD010 = "VSTHRD010:Invoke single-threaded types on Main thread";
-		public const string CheckedInCaller = "Checked in caller";
-		public const string VSTHRD100 = "VSTHRD100:Avoid async void methods";
-		public const string EventHandler = "Event handler";
-		public const string ExceptionHandled = "Exception handled";
-	}
-
 	enum MarkerStyleTypes
 	{
 		None,
---
+++ Codist/Suppression.cs
@@ -0,0 +1,14 @@
+﻿namespace Codist
+{
+	/// <summary>
+	/// Defines checkId and justifications for <see cref="System.Diagnostics.CodeAnalysis.SuppressMessageAttribute.SuppressMessageAttribute"/>.
+	/// </summary>
+	public static class Suppression
+	{
+		public const string VSTHRD010 = "VSTHRD010:Invoke single-threaded types on Main thread";
+		public const string CheckedInCaller = "Checked in caller";
+		public const string VSTHRD100 = "VSTHRD100:Avoid async void methods";
+		public const string EventHandler = "Event handler";
+		public const string ExceptionHandled = "Exception handled";
+	}
+}
