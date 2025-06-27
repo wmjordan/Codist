a66bee70bfef026363aa62d4c835fd007dcfcc9f
WMJ <wmj@live.cn>
2025-06-27 11:55:39 +0800

    - Fix adaptive Quick Info background color

--- Codist/QuickInfo/QuickInfoBackgroundController.cs
+++ Codist/QuickInfo/QuickInfoBackgroundController.cs
@@ -14,25 +14,22 @@ namespace Codist.QuickInfo
 	{
 		#region Adaptive background brightness
 		static readonly IClassificationFormatMap __ToolTipFormatMap = InitFormatMap();
-		static bool __Init;
+		static SolidColorBrush __Background;
 
 		static IClassificationFormatMap InitFormatMap() {
 			var m = ServicesHelper.Instance.ClassificationFormatMap.GetClassificationFormatMap("tooltip");
-			m.ClassificationFormatMappingChanged += (s, args) => AdaptBackgroundBrightness(__ToolTipFormatMap);
+			m.ClassificationFormatMappingChanged += (s, args) => UpdateBackgroundBrush();
 			return m;
 		}
 
-		static void AdaptBackgroundBrightness(IClassificationFormatMap formatMap) {
-			QuickInfoConfig c = Config.Instance.QuickInfo;
-			if (c.BackColor.A != 0
-				&& formatMap.DefaultTextProperties.ForegroundBrush is SolidColorBrush b
-				&& b.Color.IsDark() == c.BackColor.IsDark()) {
-				c.BackColor = c.BackColor.InvertBrightness();
+		static void AdaptBrightnessOfBackgroundColor(ref Color bc) {
+			if (bc.A != 0
+					&& __ToolTipFormatMap.DefaultTextProperties.ForegroundBrush is SolidColorBrush b
+					&& b.Color.IsDark() == bc.IsDark()) {
+				bc = bc.InvertBrightness();
 			}
 		}
 		#endregion
-
-		SolidColorBrush _Background;
 
 		public QuickInfoBackgroundController() {
 			UpdateBackgroundBrush();
@@ -41,37 +38,31 @@ namespace Codist.QuickInfo
 
 		void ConfigUpdated(ConfigUpdatedEventArgs args) {
 			if (args.UpdatedFeature.MatchFlags(Features.SuperQuickInfo)) {
-				AdaptBackgroundBrightness(__ToolTipFormatMap);
 				UpdateBackgroundBrush();
 			}
 		}
 
-		void UpdateBackgroundBrush() {
-			if (__Init == false) {
-				__Init = true;
-				AdaptBackgroundBrightness(__ToolTipFormatMap);
-			}
+		static void UpdateBackgroundBrush() {
 			var bc = Config.Instance.QuickInfo.BackColor;
 			var o = bc.A;
 			if (o != 0) {
-				if (_Background == null) {
-					_Background = new SolidColorBrush(bc.Alpha(Byte.MaxValue));
-				}
-				else {
-					_Background.Color = bc.Alpha(Byte.MaxValue);
-				}
+				AdaptBrightnessOfBackgroundColor(ref bc);
+
+				var b = new SolidColorBrush(bc.Alpha(Byte.MaxValue));
 				if (o > 0) {
-					_Background.Opacity = o / 255d;
+					b.Opacity = o / 255d;
 				}
+				b.Freeze();
+				__Background = b;
 			}
 			else {
-				_Background = null;
+				__Background = null;
 			}
 		}
 
 		protected override Task<QuickInfoItem> GetQuickInfoItemAsync(IAsyncQuickInfoSession session, CancellationToken cancellationToken) {
-			var result = _Background != null
-				? new QuickInfoItem(null, new BackgroundControllerInfoBlock(_Background))
+			var result = __Background != null
+				? new QuickInfoItem(null, new BackgroundControllerInfoBlock(__Background))
 				: null;
 			return Task.FromResult(result);
 		}
