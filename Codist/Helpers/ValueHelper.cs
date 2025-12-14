5b49024d3309247437503fb5d0e1e4d1fd1af484
WMJ <wmj@live.cn>
2025-12-14 20:30:01 +0800

    + Add missing class

---
+++ Codist/Helpers/ValueHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Codist
+{
+	static class ValueHelper
+	{
+		public static IImmutableSet<T> MakeImmutableSet<T>(this IEnumerable<T> values) {
+			return values is null ? null : (IImmutableSet<T>)ImmutableHashSet.CreateRange(values);
+		}
+	}
+}
