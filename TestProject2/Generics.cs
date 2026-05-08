556b7cf8c0b90ee7b2ca53f5961cf95960cdd738
WMJ <wmj@live.cn>
2026-05-08 21:06:00 +0800

    + Add test case for generics

--- TestProject2/Generics.cs
+++ TestProject2/Generics.cs
@@ -1,6 +1,33 @@
 ﻿using System.Collections.Generic;
+using IntDict = System.Collections.Generic.Dictionary<int, object>;
+using StringDict = System.Collections.Generic.Dictionary<string, object>;
 
 namespace TestProject.Generics;
+
+class GenericReferenceTest
+{
+	static void UseStringDictionary() {
+		var d = new StringDict();
+		ReferenceMember(d);
+	}
+
+	static void UseIntDictionary() {
+		var d = new IntDict();
+		ReferenceMember(d);
+	}
+
+	static void ReferenceMember<TKey, TValue>(Dictionary<TKey, TValue> d) {
+		var c = d.Count;
+		foreach (var item in d) {
+			var s = item.Key;
+			var o = item.Value;
+		}
+	}
+}
+
+class IntDictionary : IntDict { }
+class LongDictionary : Dictionary<long, object> { }
+class StringDictionary : StringDict { }
 
 [ApiVersion(13)]
 public class RefStructConstraint<T> where T : allows ref struct
