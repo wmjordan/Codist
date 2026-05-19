038345b85903ddcc3e91346883baf011f39f90a4
WMJ <wmj@live.cn>
2026-05-19 21:10:27 +0800

    ! Provide themed context menu for ThemedTextBox

--- Codist/Controls/ThemedTextBox.cs
+++ Codist/Controls/ThemedTextBox.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using Microsoft.VisualStudio.Shell;
 
 namespace Codist.Controls
@@ -10,6 +11,28 @@ namespace Codist.Controls
 		public ThemedTextBox() {
 			BorderThickness = new Thickness(0, 0, 0, 1);
 			this.ReferenceStyle(VsResourceKeys.TextBoxStyleKey);
+			ContextMenu = new();
+			ContextMenuOpening += ThemedTextBox_ContextMenuOpening;
+		}
+
+		void ThemedTextBox_ContextMenuOpening(object sender, ContextMenuEventArgs e) {
+			var m = ContextMenu;
+			if (m.HasItems) {
+				m.IsOpen = true;
+				return;
+			}
+			m.PlacementTarget = this;
+			m.Resources = SharedDictionaryManager.ContextMenu;
+			m.Items.AddRange(
+				new MenuItem { Command = ApplicationCommands.Cut, Icon = VsImageHelper.GetImage(IconIds.Cut) },
+				new MenuItem { Command = ApplicationCommands.Copy, Icon = VsImageHelper.GetImage(IconIds.Copy) },
+				new MenuItem { Command = ApplicationCommands.Paste, Icon = VsImageHelper.GetImage(IconIds.Paste) },
+				new Separator(),
+				new MenuItem { Command = ApplicationCommands.Delete, Icon = VsImageHelper.GetImage(IconIds.Delete) }
+			);
+			m.MinWidth = 180;
+			m.IsOpen = true;
+			e.Handled = true;
 		}
 	}
 }
